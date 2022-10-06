747cc232917d777633c9edd6237729c72f1e3524
elbouchouki-wsl <elbouchoukigamer@gmail.com>
2022-10-06 14:47:41 +0100

    init

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Elbouchouki's portfolio</title>
+      </Head>
+      <h1 className="bg-red-700">tester</h1>
+    </div>
+  );
+};
+
+export default Home;
